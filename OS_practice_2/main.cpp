2a5d56bab922cf1807c7e5ae2ddf7591b6176637
Костя Беспалов <kostanus2012@gmail.com>
2022-03-14 03:34:17 +0300

    v4.0

--- OS_practice_2/main.cpp
+++ OS_practice_2/main.cpp
@@ -1,21 +1,21 @@
-#include <stdint.h>
+#include <iostream>
 #include <thread>
 #include <mutex>
-#include <iostream>
-#include <iomanip>
-#include <sstream>
+#include <string>
 #include <Windows.h>
+#include <ctime>
+#include <fstream>
 
 class hash_int32 {
 public:
-	uint32_t h0;
-	uint32_t h1;
-	uint32_t h2;
-	uint32_t h3;
-	uint32_t h4;
-	uint32_t h5;
-	uint32_t h6;
-	uint32_t h7;
+	unsigned int h0;
+	unsigned int h1;
+	unsigned int h2;
+	unsigned int h3;
+	unsigned int h4;
+	unsigned int h5;
+	unsigned int h6;
+	unsigned int h7;
 	hash_int32(std::string hash) {
 		h0 = stoul(hash.substr(0, 8), 0, 16);
 		h1 = stoul(hash.substr(8, 8), 0, 16);
@@ -26,7 +26,7 @@ public:
 		h6 = stoul(hash.substr(48, 8), 0, 16);
 		h7 = stoul(hash.substr(56, 8), 0, 16);
 	}
-	hash_int32(uint32_t n0, uint32_t n1, uint32_t n2, uint32_t n3, uint32_t n4, uint32_t n5, uint32_t n6, uint32_t n7) {
+	hash_int32(unsigned int n0, unsigned int n1, unsigned int n2, unsigned int n3, unsigned int n4, unsigned int n5, unsigned int n6, unsigned int n7) {
 		h0 = n0;
 		h1 = n1;
 		h2 = n2;
@@ -41,18 +41,19 @@ bool operator == (hash_int32 a, hash_int32 b) {
 	return { a.h0 == b.h0 and a.h1 == b.h1 and a.h2 == b.h2 and a.h3 == b.h3 and a.h4 == b.h4 and a.h5 == b.h5 and a.h6 == b.h6 and a.h7 == b.h7 };
 }
 
-void work(uint16_t number, uint16_t amount, hash_int32 hash);
-hash_int32 sha256(uint8_t pass[6]);
-uint32_t rightrotate(uint32_t w, uint8_t s);
+void work(unsigned short number, unsigned short amount, hash_int32 hash);
+hash_int32 sha256(unsigned char pass[6]);
+unsigned int rightrotate(unsigned int w, unsigned char s);
 
 std::mutex cout_guard;
-uint8_t pass[11881376][6];
+unsigned char pass[11881376][6];
+bool found;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	for (uint32_t i = 0; i < 11881376; i++) {
+	for (unsigned int i = 0; i < 11881376; i++) {
 		pass[i][0] = i / 26 / 26 / 26 / 26 % 26 + 97;
 		pass[i][1] = i / 26 / 26 / 26 % 26 + 97;
 		pass[i][2] = i / 26 / 26 % 26 + 97;
@@ -61,30 +62,74 @@ int main() {
 		pass[i][5] = 0;
 	}
 
-	std::string hash;
-	std::cout << "Введите хеш: ";
-	std::cin >> hash;
+	for (;;) {
+		unsigned short choise = 0;
+		while (choise < 1 or 3 < choise) {
+			std::cout << "Прочитать хэш из:\n 1. консоли\n 2. файла\n 3. (выход)\n>> ";
+			std::cin >> choise;
+		}
 
-	hash_int32 hash_int(hash);
-	
-	uint16_t amount;
-	std::cout << "Введите количество потоков для перебора: ";
-	std::cin >> amount;
+		std::string hash;
 
-	std::thread* worker = new std::thread[amount];
-	for (uint32_t i = 0; i < amount; i++) {
-		worker[i] = std::thread(work, i, amount, hash_int);
-	}
-	for (uint32_t i = 0; i < amount; i++) {
-		worker[i].join();
+		if (choise == 1) {
+			std::cout << "Введите хеш пятибуквенного пароля: ";
+			std::cin >> hash;
+		} else if (choise == 2) {
+			std::string path;
+			std::cout << "Введите полный путь к файлу: ";
+			std::cin >> path;
+			for (int i = 0; i < path.size(); i++) {
+				if (path[i] == '\\') {
+					path[i] = '/';
+				}
+			}
+			
+			std::ifstream fin(path);
+			while (!fin) {
+				std::cout << "Файл " << path.substr(path.find_last_of('/') + 1) << " не найден по указанному пути!\nВведите полный путь к файлу: ";
+				std::cin >> path;
+				for (int i = 0; i < path.size(); i++) {
+					if (path[i] == '\\') {
+						path[i] = '/';
+					}
+				}
+				fin.open(path);
+			}
+
+			std::getline(fin, hash);
+		} else if (choise == 3) {
+			break;
+		}
+
+		hash_int32 hash_int(hash);
+
+		unsigned short amount;
+		std::cout << "Введите количество потоков для перебора: ";
+		std::cin >> amount;
+
+		found = 0;
+
+		clock_t ts = clock();
+		std::thread* worker = new std::thread[amount];
+		for (unsigned int i = 0; i < amount; i++) {
+			worker[i] = std::thread(work, i, amount, hash_int);
+		}
+		for (unsigned int i = 0; i < amount; i++) {
+			worker[i].join();
+		}
+		if (!found) {
+			std::cout << "Нет подходящего пароля\n";
+		}
+		std::cout << "Перебор занял " << ((float)(clock() - ts)) / CLOCKS_PER_SEC << " секунд\n\n";
+
 	}
 
 	return 0;
 }
 
-void work(uint16_t number, uint16_t amount, hash_int32 hash) {
+void work(unsigned short number, unsigned short amount, hash_int32 hash) {
 
-	uint32_t from = 11881376 / amount * number, to = 11881376 / amount * (number + 1);
+	unsigned int from = 11881376 / amount * number, to = 11881376 / amount * (number + 1);
 	if (number < 11881376 % amount) {
 		from = from + number;
 		to = to + number + 1;
@@ -94,18 +139,22 @@ void work(uint16_t number, uint16_t amount, hash_int32 hash) {
 		to = to + 11881376 % amount;
 	}
 
-	for (uint32_t i = from; i < to; i++) {
+	for (unsigned int i = from; i < to; i++) {
 		if (hash == sha256(pass[i])) {
 			cout_guard.lock();
-			std::cout << pass[i] << '\n';
+			std::cout << "Искомый пароль: " << pass[i] << '\n';
 			cout_guard.unlock();
+			found = 1;
+		}
+		if (found) {
+			break;
 		}
 	}
 
 }
 
-hash_int32 sha256(uint8_t pass[6]) {
-	uint8_t s[64] = {
+hash_int32 sha256(unsigned char pass[6]) {
+	unsigned char s[64] = {
 		pass[0], pass[1], pass[2], pass[3], pass[4], 128, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0,
@@ -116,16 +165,16 @@ hash_int32 sha256(uint8_t pass[6]) {
 		0, 0, 0, 0, 0, 0, 0, 40,
 	};
 
-	uint32_t h0 = 0x6a09e667;
-	uint32_t h1 = 0xbb67ae85;
-	uint32_t h2 = 0x3c6ef372;
-	uint32_t h3 = 0xa54ff53a;
-	uint32_t h4 = 0x510e527f;
-	uint32_t h5 = 0x9b05688c;
-	uint32_t h6 = 0x1f83d9ab;
-	uint32_t h7 = 0x5be0cd19;
+	unsigned int h0 = 0x6a09e667;
+	unsigned int h1 = 0xbb67ae85;
+	unsigned int h2 = 0x3c6ef372;
+	unsigned int h3 = 0xa54ff53a;
+	unsigned int h4 = 0x510e527f;
+	unsigned int h5 = 0x9b05688c;
+	unsigned int h6 = 0x1f83d9ab;
+	unsigned int h7 = 0x5be0cd19;
 
-	uint32_t k[64] = {
+	unsigned int k[64] = {
 		0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
 		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
 		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -136,7 +185,7 @@ hash_int32 sha256(uint8_t pass[6]) {
 		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 	};
 
-	uint32_t w[64] = {
+	unsigned int w[64] = {
 		0, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0,
@@ -146,30 +195,30 @@ hash_int32 sha256(uint8_t pass[6]) {
 		0, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0
 	};
-	for (uint32_t i = 0; i < 16; i++) w[i] = (s[4 * i] << 24) + (s[4 * i + 1] << 16) + (s[4 * i + 2] << 8) + s[4 * i + 3];
+	for (unsigned int i = 0; i < 16; i++) w[i] = (s[4 * i] << 24) + (s[4 * i + 1] << 16) + (s[4 * i + 2] << 8) + s[4 * i + 3];
 
-	for (uint32_t i = 16; i < 64; i++) {
-		uint32_t s0 = rightrotate(w[i - 15], 7) ^ rightrotate(w[i - 15], 18) ^ (w[i - 15] >> 3);
-		uint32_t s1 = rightrotate(w[i - 2], 17) ^ rightrotate(w[i - 2], 19) ^ (w[i - 2] >> 10);
+	for (unsigned int i = 16; i < 64; i++) {
+		unsigned int s0 = rightrotate(w[i - 15], 7) ^ rightrotate(w[i - 15], 18) ^ (w[i - 15] >> 3);
+		unsigned int s1 = rightrotate(w[i - 2], 17) ^ rightrotate(w[i - 2], 19) ^ (w[i - 2] >> 10);
 		w[i] = w[i - 16] + s0 + w[i - 7] + s1;
 	}
 
-	uint32_t a = h0;
-	uint32_t b = h1;
-	uint32_t c = h2;
-	uint32_t d = h3;
-	uint32_t e = h4;
-	uint32_t f = h5;
-	uint32_t g = h6;
-	uint32_t h = h7;
+	unsigned int a = h0;
+	unsigned int b = h1;
+	unsigned int c = h2;
+	unsigned int d = h3;
+	unsigned int e = h4;
+	unsigned int f = h5;
+	unsigned int g = h6;
+	unsigned int h = h7;
 
 	for (int i = 0; i < 64; i++) {
-		uint32_t s1 = rightrotate(e, 6) ^ rightrotate(e, 11) ^ rightrotate(e, 25);
-		uint32_t ch = (e & f) ^ ((~e) & g);
-		uint32_t temp1 = h + s1 + ch + k[i] + w[i];
-		uint32_t s0 = rightrotate(a, 2) ^ rightrotate(a, 13) ^ rightrotate(a, 22);
-		uint32_t maj = (a & b) ^ (a & c) ^ (b & c);
-		uint32_t temp2 = s0 + maj;
+		unsigned int s1 = rightrotate(e, 6) ^ rightrotate(e, 11) ^ rightrotate(e, 25);
+		unsigned int ch = (e & f) ^ ((~e) & g);
+		unsigned int temp1 = h + s1 + ch + k[i] + w[i];
+		unsigned int s0 = rightrotate(a, 2) ^ rightrotate(a, 13) ^ rightrotate(a, 22);
+		unsigned int maj = (a & b) ^ (a & c) ^ (b & c);
+		unsigned int temp2 = s0 + maj;
 		h = g;
 		g = f;
 		f = e;
@@ -192,6 +241,6 @@ hash_int32 sha256(uint8_t pass[6]) {
 	return hash_int32( h0, h1, h2, h3, h4, h5, h6, h7 );
 }
 
-uint32_t rightrotate(uint32_t w, uint8_t s) {
+unsigned int rightrotate(unsigned int w, unsigned char s) {
 	return (w >> s) + (w << (32 - s));
 }
